6292e08a50d5c3724e576d59ea146fdaa40ecdfc
JDias6 <>
2025-10-26 19:25:50 +0000

    modified settings.py version 2

--- timesheet_app/settings.py
+++ timesheet_app/settings.py
@@ -87,7 +87,13 @@ if ENV_STATE == "production":
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # Customise User model
 AUTH_USER_MODEL = "timesheet.User"
